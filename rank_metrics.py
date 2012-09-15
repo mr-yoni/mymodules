77fefee51e0dbf74ead0c6f08f42842df57985db
Brandyn A. White <bwhite@dappervision.com>
2012-09-15 02:10:39 -0400

    Updated
    Signed-off-by: Brandyn A. White <bwhite@dappervision.com>

--- rank_metrics.py
+++ rank_metrics.py
@@ -1,22 +1,149 @@
+"""Information Retrieval metrics
+
+Useful Resources:
+http://www.cs.utexas.edu/~mooney/ir-course/slides/Evaluation.ppt
+http://www.nii.ac.jp/TechReports/05-014E.pdf
+Learning to Rank for Information Retrieval (Tie-Yan Liu)
+"""
+import numpy as np
+
+
 def mean_reciprocal_rank(rs):
     """Score is reciprocal of the rank of the first relevant item
 
-    First element is "rank 1" so as to not result in infinity.  Relevance is
-    binary (nonzero is relevant).
+    First element is 'rank 1'.  Relevance is binary (nonzero is relevant).
 
     Example from http://en.wikipedia.org/wiki/Mean_reciprocal_rank
     >>> rs = [[0, 0, 1], [0, 1, 0], [1, 0, 0]]
     >>> mean_reciprocal_rank(rs)
-    0.6111111111111112
+    0.61111111111111105
+    >>> rs = np.array([[0, 0, 0], [0, 1, 0], [1, 0, 0]])
+    >>> mean_reciprocal_rank(rs)
+    0.5
+    >>> rs = [[0, 0, 0, 1], [1, 0, 0], [1, 0, 0]]
+    >>> mean_reciprocal_rank(rs)
+    0.75
 
     Args:
-        rs: List of relevance scores in rank order (first element is the first item)
+        rs: Iterator of relevance scores (list or numpy) in rank order
+            (first element is the first item)
 
     Returns:
         Mean reciprocal rank
     """
-    return np.mean([1. / (np.asfarray(r).nonzero()[0] + 1) for r in rs])
+    rs = (np.asarray(r).nonzero()[0] for r in rs)
+    return np.mean([1. / (r[0] + 1) if r.size else 0. for r in rs])
 
 
-def rank_precision_k():
-    pass
+def r_precision(r):
+    """Score is precision after all relevant documents have been retrieved
+
+    Relevance is binary (nonzero is relevant).
+
+    >>> r = [0, 0, 1]
+    >>> r_precision(r)
+    0.33333333333333331
+    >>> r = [0, 1, 0]
+    >>> r_precision(r)
+    0.5
+    >>> r = [1, 0, 0]
+    >>> r_precision(r)
+    1.0
+
+    Args:
+        r: Relevance scores (list or numpy) in rank order
+            (first element is the first item)
+
+    Returns:
+        R Precision
+    """
+    r = np.asarray(r) != 0
+    z = r.nonzero()[0]
+    if not z.size:
+        return 0.
+    return np.mean(r[:z[-1] + 1])
+
+
+def precision_at_k(r, k):
+    """Score is precision @ k
+
+    Relevance is binary (nonzero is relevant).
+
+    >>> r = [0, 0, 1]
+    >>> precision_at_k(r, 1)
+    0.0
+    >>> precision_at_k(r, 2)
+    0.0
+    >>> precision_at_k(r, 3)
+    0.33333333333333331
+    >>> precision_at_k(r, 4)
+    Traceback (most recent call last):
+        File "<stdin>", line 1, in ?
+    ValueError: Relevance score length < k
+
+
+    Args:
+        r: Relevance scores (list or numpy) in rank order
+            (first element is the first item)
+
+    Returns:
+        Precision @ k
+
+    Raises:
+        ValueError: len(r) must be >= k
+    """
+    assert k >= 1
+    r = np.asarray(r) != 0
+    if r.size < k:
+        raise ValueError('Relevance score length < k')
+    return np.mean(r[:k])
+
+
+def average_precision(r):
+    """Score is average precision (area under PR curve)
+
+    Relevance is binary (nonzero is relevant).
+
+    >>> r = [1, 1, 0, 1, 0, 1, 0, 0, 0, 1]
+    >>> delta_r = 1. / sum(r)
+    >>> sum([sum(r[:x + 1]) / (x + 1.) * delta_r for x, y in enumerate(r) if y])
+    0.7833333333333333
+    >>> average_precision(r)
+    0.78333333333333333
+
+    Args:
+        r: Relevance scores (list or numpy) in rank order
+            (first element is the first item)
+
+    Returns:
+        Average precision
+    """
+    r = np.asarray(r) != 0
+    return np.nan_to_num(np.mean([precision_at_k(r, k + 1) for k in range(r.size) if r[k]]))
+
+
+def mean_average_precision(rs):
+    """Score is mean average precision
+
+    Relevance is binary (nonzero is relevant).
+
+    >>> rs = [[1, 1, 0, 1, 0, 1, 0, 0, 0, 1]]
+    >>> mean_average_precision(rs)
+    0.78333333333333333
+    >>> rs = [[1, 1, 0, 1, 0, 1, 0, 0, 0, 1], [0]]
+    >>> mean_average_precision(rs)
+    0.39166666666666666
+
+    Args:
+        rs: Iterator of relevance scores (list or numpy) in rank order
+            (first element is the first item)
+
+    Returns:
+        Mean average precision
+    """
+    return np.mean([average_precision(r) for r in rs])
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
